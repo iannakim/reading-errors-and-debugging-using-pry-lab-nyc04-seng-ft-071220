920923b71a6694986e03929c909ce0fddf5491f3
Hyun Ji  Kim <dear.hyunji@gmail.com>
2020-07-15 16:14:04 +0000

    Automatically backed up by Learn

--- fix_using_tests/pemdos.rb
+++ fix_using_tests/pemdos.rb
@@ -2,9 +2,9 @@ require 'pry'
 
 def snake_it_up(string)
   if string[0] == "s"
-    # binding.pry
+  new_string = string
     10.times do
-      string[0]
+      new_string = "s" + new_string
     end
   else
     string
